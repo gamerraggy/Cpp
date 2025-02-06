eaba88e3dc7cc014dd19eb6004a546443a33d71d
gamerraggy <javib0414@gmail.com>
2025-02-06 10:15:42 -0700

    Create P2.py

---
+++ Daily Codes/2-6-25/P2.py
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+bool isVowel(char x);
+
+int main() {
+	
+	isVowel('a');
+
+
+
+}
+
+bool isVowel(char x) {
+	
+	return (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u');
+
+
+
+
+}
